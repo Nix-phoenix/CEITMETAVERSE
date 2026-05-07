e8c8ebd26de8fba247512e3566689766ec3a25e5
unknown <khamsavanhvanvilay992@gmail.com>
2026-05-07 23:39:57 +0700

    run prisma via node to fix permission denied

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 cd Script/DataBase
 npm install
-./node_modules/.bin/prisma generate --schema=./prisma/schema.prisma
-./node_modules/.bin/prisma migrate deploy --schema=./prisma/schema.prisma
+node ./node_modules/.bin/prisma generate --schema=./prisma/schema.prisma
+node ./node_modules/.bin/prisma migrate deploy --schema=./prisma/schema.prisma
